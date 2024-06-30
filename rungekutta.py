fa8d3bd463e12b950673cec357b9f924364cd453
fragherazzi90162 <90162@sistemas.frc.utn.edu.ar>
2024-06-29 23:54:23 -0300

    Tabla Runge Kutta mostrada. Agregar boton en la otra interfaz para mostrarla

--- rungekutta.py
+++ rungekutta.py
@@ -1,30 +1,6 @@
+import sys
 import random
-
-
-# Runge kutta 
-def rungeKutta(t):
-
-    h = 0.01
-    to = 0
-    Co = t
-    tiempoEnfriamento = 0
-
-    while Co >= 0:
-        
-
-        k1 = (0.025*to)-(0.5*Co)-(12.85)
-        k2 = (0.025*(to+(h/2)))-(0.5*(Co+(h/2)*k1))-(12.85)
-        k3 = (0.025*(to+(h/2)))-(0.5*(Co+(h/2)*k2))-(12.85)
-        k4 = (0.025*(to+(h)))-(0.5*(Co+(h)*k3))-(12.85)
-
-        to = to+h
-        Co = Co+(h/6)*(k1+2*k2+2*k3+k4)
-
-    
-    tiempoEnfriamento = ((to*30)/60)
-
-    return tiempoEnfriamento
-        
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget
 
 # Funcion que establece a que hora se realiza el corte
 def relojCorte(t): 
@@ -44,3 +20,75 @@ def relojCorte(t):
         return 6*t
     else:
         return 8*t
+
+class RungeKuttaApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Runge-Kutta Method")
+        self.setGeometry(100, 100, 800, 600)
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(8)
+        self.table.setHorizontalHeaderLabels(['t','C' ,'k1', 'k2', 'k3', 'k4', 't(i+1)', 'C(i+1)'])
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.table)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+        self.rungeKutta(1000) # Aca poner el t correspondiente
+
+    def inicializar(self, values):
+            rowPosition = self.table.rowCount()
+            self.table.insertRow(rowPosition)
+            for i, value in enumerate(values):
+                self.table.setItem(rowPosition, i, QTableWidgetItem(str(value)))
+
+    def rungeKutta(self, t):
+        h = 0.01
+        to = 0
+        Co = t
+        tiempoEnfriamento = 0
+
+        self.inicializar(['', '', '', '', '', '', to, Co])
+
+        while Co >= 0:
+            k1 = (0.025 * to) - (0.5 * Co) - 12.85
+            k2 = (0.025 * (to + (h / 2))) - (0.5 * (Co + (h / 2) * k1)) - 12.85
+            k3 = (0.025 * (to + (h / 2))) - (0.5 * (Co + (h / 2) * k2)) - 12.85
+            k4 = (0.025 * (to + h)) - (0.5 * (Co + h * k3)) - 12.85
+
+            to = to + h
+            Co = Co + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+
+            k1 = round(k1, 4)
+            k2 = round(k2, 4)
+            k3 = round(k3, 4)
+            k4 = round(k4, 4)
+
+            to = round(to, 4)
+            Co = round(Co, 4)
+            vector = [to, Co, k1, k2, k3, k4, round(to + h,4), round(Co + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4),4)] 
+            self.llenarTabla(vector)
+        
+        tiempoEnfriamento = ((to * 30) / 60)
+        return tiempoEnfriamento
+
+    def llenarTabla(self, vector):
+        rowPosition = self.table.rowCount()
+        self.table.insertRow(rowPosition)
+
+        for i, value in enumerate(vector):
+            self.table.setItem(rowPosition, i, QTableWidgetItem(str(value)))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = RungeKuttaApp()
+    ex.show()
+    sys.exit(app.exec_())
